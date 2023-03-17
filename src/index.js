24237d6ef870970c4d757a1dda69ae793f1805f7
Julia-Kaim <julia.kaim08@gmail.com>
2023-03-17 22:03:19 +0100

    corrected

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./css/styles.css";
 import debounce from "lodash.debounce";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
-import { fetchCountries } from "./fetchCountires";
+import { fetchCountries } from "./fetchCountries";
 
 const DEBOUNCE_DELAY = 300;
 
@@ -10,34 +10,6 @@ const list = document.querySelector(".country-list");
 const infoAboutCountries = document.querySelector(".country-info");
 
 const cleanMarkup = (ref) => (ref.innerHTML = "");
-
-const renderMarkup = (data) => {
-	if (data.length === 1) {
-		cleanMarkup(list);
-		const markupInfo = createInfoMarkup(data);
-		infoAboutCountries.innerHTML = markupInfo;
-	} else {
-		cleanMarkup(infoAboutCountries);
-		const markupList = createMarkupList(data);
-		list.innerHTML = markupList;
-	}
-};
-
-const createMarkupList = (data) => {
-	return data
-		.map(
-			({ name, flags }) =>
-				'<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>',
-		)
-		.join("");
-};
-
-const createInfoMarkup = (data) => {
-	return data.map(
-		({ name, capital, population, flags, languages }) =>
-			'<h1><img src="${flags.png}" alt="${name.official}" width="40" height="40">${name.official}</h1><p>Capital: ${capital}</p><p>Population: ${population}</p><p>Languages: ${Object.values(languages)}</p>',
-	);
-};
 
 const inputEvent = (e) => {
 	const textInput = e.target.value.trim();
@@ -63,6 +35,38 @@ const inputEvent = (e) => {
 			cleanMarkup(infoAboutCountries);
 			Notify.failure("Oops, there is no country with that name");
 		});
+};
+const renderMarkup = (data) => {
+	if (data.length === 1) {
+		cleanMarkup(list);
+		const markupInfo = createInfoMarkup(data);
+		infoAboutCountries.innerHTML = markupInfo;
+	} else {
+		cleanMarkup(infoAboutCountries);
+		const markupList = createMarkupList(data);
+		list.innerHTML = markupList;
+	}
+};
+
+const createMarkupList = (data) => {
+	return data
+		.map(
+			({ name, flags }) =>
+				`<li><img src="${flags.png}" alt="${name.official}" width="60" height="40"> ${name.official}</li>`,
+		)
+		.join("");
+};
+
+const createInfoMarkup = (data) => {
+	return data.map(
+		({ name, capital, population, flags, languages }) =>
+			`<h1><img src="${flags.png}" alt="${
+				name.official
+			}" width="40" height="40">${name.official}</h1>
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p>`,
+	);
 };
 
 input.addEventListener("input", debounce(inputEvent, DEBOUNCE_DELAY));
